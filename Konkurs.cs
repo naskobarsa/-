00d9fe9ffa7a7dd60f9cc9170924e0e85650236a
naskobarsa <32620407+naskobarsa@users.noreply.github.com>
2018-01-09 22:10:54 +0200

    Add project files.

---
+++ Konkurs.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_03___Truck_Driver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var dancers = int.Parse(Console.ReadLine());
+            var points = double.Parse(Console.ReadLine());           
+            string season = Console.ReadLine();
+            string country = Console.ReadLine();
+
+            var total = 0.00;
+
+            if (country == "Bulgaria")
+                
+            {
+                if (season == "summer")
+
+                {
+                    total = dancers * points*0.95;
+                }
+                else if (season == "winter")
+                {
+                    total = dancers * points*0.92;
+                }
+
+            }
+
+            
+            else
+                
+            {
+                if (season == "summer")
+                {
+                    total = (dancers * points) * 1.5*0.90;
+                }
+                else if (season == "winter")
+                {
+                    total = (dancers * points) * 1.5*0.85;
+                }
+            } 
+            
+
+
+        }  
+    }
+}
---
+++ metric/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
